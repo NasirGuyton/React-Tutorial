cbdec399a42a8c388d11054c41a517ffb4389eec
Student Guest <studentguest@Pauls-MacBook-Air.local>
2025-03-01 14:20:58 -0800

    Added dynamic data with JSON and hours

--- src/POPOSList.js
+++ src/POPOSList.js
@@ -1,54 +1,20 @@
-// src/POPOSList.js
-import './POPOSList.css';
+import React from 'react';
 import POPOSSpace from './POPOSSpace';
-
-
-const spaces = [
-  {
-    name: "50 California Street",
-    address: "50 California St.",
-    image: "50-california-st.jpg",
-  },
-  {
-    name: "100 Pine Street",
-    address: "100 Pine St.",
-    image: "100-pine.jpg",
-  },
-  {
-    name: "101 California Street",
-    address: "101 California St.",
-    image: "101-california.jpg",
-  },
-  {
-    name: "343 Sansome Roof Garden",
-    address: "343 Sansome St.",
-    image: "343-sansome-roof-garden.jpg",
-  },
-  {
-    name: "525 Market Street Plaza",
-    address: "525 Market St.",
-    image: "525-market-street-plaza.jpg",
-  },
-  {
-    name: "555 California Street",
-    address: "555 California St.",
-    image: "555-california.jpg",
-  }
-];
+import './POPOSList.css';
+import data from './sfpopos-data.json';
 
 function POPOSList() {
-  return (
-    <div className="POPOSList">
-      {spaces.map((space, index) => (
-        <POPOSSpace 
-          key={index}
-          name={space.name}
-          address={space.address}
-          image={space.image}
-        />
-      ))}
-    </div>
-  );
+  const spaces = data.map(({ title, address, images, hours }) => (
+    <POPOSSpace
+      key={title}  // Unique key for React optimization
+      name={title}
+      address={address}
+      image={images[0]}  // Use the first image in the array
+      hours={hours}  // Display opening hours
+    />
+  ));
+
+  return <div className="POPOSList">{spaces}</div>;
 }
 
 export default POPOSList;
--- src/POPOSSpace.js
+++ src/POPOSSpace.js
@@ -1,22 +1,23 @@
 // src/POPOSSpace.js
+import React from 'react';
 import './POPOSSpace.css';
 
-function POPOSSpace(props) {
-    const { name, image, address } = props;
-  
-    return (
-      <div className="POPOSSpace">
-        <img
-          src={`${process.env.PUBLIC_URL}/images/${image}`}
-          width="300"
-          height="300"
-          alt={name}
-        />
-        <h1>{name}</h1>
-        <div>{address}</div>
-      </div>
-    );
-  }
-  
-  export default POPOSSpace;
+function POPOSSpace({ name, image, address, hours }) {
+  return (
+    <div className="POPOSSpace">
+      <img
+        src={`${process.env.PUBLIC_URL}/images/${image}`}
+        width="300"
+        height="300"
+        alt={name}
+      />
+      <h1>{name}</h1>
+      <div>{address}</div>
+      <div className="POPOSSpace-hours">{hours}</div>
+    </div>
+  );
+}
+
+export default POPOSSpace;
+
   
